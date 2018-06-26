a5b1c485466db97c9003c5845fad6e06fac606a7
zidane168 <huuvi168@gmail.com>
2018-06-26 22:08:48 +0700

    - Create MVC AngularJS projects!

---
+++ OnlineShopDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace OnlineShopDemo
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+            // added by vi.ly
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular-1.4.9.js",
+                        "~/Scripts/angular-route.min-1.4.9.js" ));
+
+
+
+              //<script src="s"> </script>
+              //  <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.4.9/angular-route.min.js"> </script>
+              //  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-animate.js"> </script>
+        }
+    }
+}
---
+++ OnlineShopDemo/Scripts/MyApp.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular-1.4.9.js" />
+
+
+(function () {
+
+    // create a module    
+    var app = angular.module('MyApp', ['ngRoute']);
+
+    // create a controller
+    app.controller('HomeController', function($scope) {
+        $scope.Message = "Tong Nian!";
+
+    });
+
+})();
+
