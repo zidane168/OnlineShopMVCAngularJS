224cacd890baadf2d2e301a88a3c2064012c1c38
zidane168 <huuvi168@gmail.com>
2018-06-30 15:37:20 +0700

    - Create temp api function call!

---
+++ OnlineShopDemo/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopDemo.Models
+{
+    public class Location
+    {
+
+        public string LocationId;
+        public string LocationName;
+
+    }
+}
